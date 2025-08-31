1288257b119caa99d1582f5fbe2113fe6d8c5246
Denmasmolano21 <putraabidumimatham@gmail.com>
2025-08-31 19:58:44 +0700

    19:58

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ const PORT = process.env.PORT || 3003
 
 const server = http.createServer(app)
 
-app.listen(PORT, '0.0.0.0', () =>
+app.listen(PORT, () =>
   console.log(`🚀 Server ready on port ${PORT}`)
 )
 
 server.listen(PORT, () => {
 	logger.info(`Server listening at http://localhost:${PORT}`);
-	logger.info(`Access the root route at http://localhost:${PORT}/hello`)
+	logger.info(`Access the root route at http://localhost:${PORT}`)
 })
